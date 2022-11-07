2359f01eefb536b523642d4f8ae48cd90f812795
mino89 <pierdomenicoreitano@gmail.com>
2022-11-07 23:50:57 +0100

    completed first section

---
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -0,0 +1,48 @@
+import { CoursesService } from './../services/courses.service';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+
+import { filter, tap } from 'rxjs/operators';
+import { CourseDialogComponent } from '../course-dialog/course-dialog.component';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'courses-card-list',
+  templateUrl: './courses-card-list.component.html',
+  styleUrls: ['./courses-card-list.component.scss']
+})
+export class CoursesCardListComponent implements OnInit {
+  @Input()
+  courses: Course[] = []
+  @Output()
+  private coursesChanged = new EventEmitter()
+  constructor(
+    private dialog: MatDialog,
+    private coursesService: CoursesService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+
+  editCourse(course: Course) {
+
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "400px";
+
+    dialogConfig.data = course;
+
+    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
+
+    dialogRef.afterClosed()
+    .pipe(
+      filter(val=> !!val),
+      tap(()=> this.coursesChanged.emit(''))
+    )
+    .subscribe()
+  }
+
+}
